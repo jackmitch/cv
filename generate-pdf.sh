af3ffca7d6cd8e8aea8db2169c0a96b3980d7216
Jack Mitchell <ml@embed.me.uk>
2022-06-22 11:14:20 +0100

    another try at correct non-cached PDF

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -1,5 +1,7 @@
 pip3 install gh-md-to-html[pdf_export]
 
+rm JackMitchell-CV.pdf
+
 ~/.local/bin/gh-md-to-html README.md -p JackMitchell-CV.pdf
 
 rm -rf github-markdown-css images github.css README.html
